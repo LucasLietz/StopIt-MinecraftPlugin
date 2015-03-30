ce79df822f597f84f20f76db6a07041adf044c43
Lucas Lietz <fateone@web.de>
2015-03-31 00:57:28 +0200

    Setup project for use

--- src/com/github/fate0608/f3rdinand/f3rdinand.java
+++ src/com/github/fate0608/f3rdinand/f3rdinand.java
@@ -1,5 +1,8 @@
 package com.github.fate0608.f3rdinand;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,10 +13,24 @@ public class f3rdinand extends JavaPlugin implements Listener{
 		getServer().getPluginManager().registerEvents(this,this);
 		this.getLogger().info("&3f3rdinand wurde erfolgreich aktiviert.");
 	}
-	
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        Player player = null;
+        if(sender instanceof Player){
+            player = (Player) sender;
+        }
+
+        if(cmd.getName().equalsIgnoreCase("testplug")){ // Wenn der Spieler /basic eingibt, dann tue das folgende...
+            sender.sendMessage("&3Das Plugin funktioniert soweit.");
+            return true;
+        } // Wenn das passiert, wird die Funktion abbrechen und true als Wert zurückgeben. Wenn nicht, dann wird false als Wert zurückgegeben.
+        return false;
+    }
+
 	@Override 
 	public void onDisable(){
-		this.getLogger().info("&4f3rdinand wurde deaktiviert.");
+
+        this.getLogger().info("&4f3rdinand wurde deaktiviert.");
 	}
 	
 }
