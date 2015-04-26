213d5fde90e3c7cb157cb1bc2741cc5e1e58684f
Lucas Lietz <fateone@web.de>
2015-04-26 22:48:07 +0200

    major changes to code - fixed several bugs, implemented ignore on user kick, added whitelist on command execution, refactored a lot

--- src/com/github/fate0608/StopIt/StopIt.java
+++ src/com/github/fate0608/StopIt/StopIt.java
@@ -17,31 +17,36 @@ public class StopIt extends JavaPlugin
     private boolean playersShouldBeKicked = false;
     private String _reason;
     private boolean _isInProcess = false;
-    private String[] _excluded;
+    private boolean _shouldWhiteListBeEnabled = this.getConfig().getBoolean("StopIt.commands.WhitelistOnProcess.messages.WhitelistOnProcess");
     ConsoleCommandSender ccs = this.getServer().getConsoleSender();
 
 	@Override
 	public void onEnable()
     {
+        _server = getServer();
         ccs.sendMessage(ChatColor.GOLD + "[StopIt] wurde erfolgreich aktiviert.");
         ccs.sendMessage("[StopIt] by f3rd");
+        _server.setWhitelist(false);
         this.reloadConfig();
-        this.getConfig().options().header("StopIt-Plugin. Hier kannst du das Plugin vollständig konfigurieren.\n Servername, " +
-                "Ts3Ip, Homepage sollten selbsterklärend sein. ExcludedPlayers sind solche, die beim " +
-                "Kick der Spieler nicht gekickt werden. Der Autostopper stoppt den Server bei true oder nicht bei false.");
+        this.getConfig().options().header("Stop-It by f3rd" +
+                "Servername = Euer Servername" +
+                "TS3-IP: Hier könnt ihr eure TS3-IP eintragen" +
+                "Homepage: Hier könnt ihr eure Homepage eintragen" +
+                "WhitelistOnProcess: true:Aktiviert die Whitelist, wenn das Plugin ausgeführt wird. false:Deaktiviert sie." +
+                "autostopper: true: Stoppt den Server nach erreichen des Countdowns.");
         this.getConfig().addDefault("StopIt.commands.servername.messages.servername","SERVERNAME HIER");
         this.getConfig().addDefault("StopIt.commands.ts3ip.messages.ts3ip","ts3ip");
         this.getConfig().addDefault("StopIt.commands.homepage.messages.homepage","HOMEPAGE HIER");
-        this.getConfig().addDefault("StopIt.commands.excludeplayer.messages.excludeplayer","");
-        this.getConfig().addDefault("StopIt.commands.autostopper.messages.autostopper","false");
+        this.getConfig().addDefault("StopIt.commands.WhitelistOnProcess.messages.WhitelistOnProcess",true);
+        this.getConfig().addDefault("StopIt.commands.autostopper.messages.autostopper",false);
         this.getConfig().options().copyDefaults(true);
         this.saveConfig();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        _server = getServer();
 
+if(sender instanceof Player){
         Player player = null;
         player = (Player) sender;
 
@@ -57,7 +62,29 @@ public class StopIt extends JavaPlugin
                 {
                     _server.broadcastMessage(ChatColor.RED + "Server-Restart abgebrochen!");
                     _server.getScheduler().cancelAllTasks();
+                    _server.setWhitelist(false);
                     _isInProcess = false;
+                    return true;
+                }
+
+                if(args.length > 0 && args[0].equalsIgnoreCase("now"))
+                {
+                    AnnounceShutdown();
+
+                    if(_shouldWhiteListBeEnabled)
+                    {
+                        SetWhitelist();
+                    }
+                    _server.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                        public void run() {
+                                for (Player p : _server.getOnlinePlayers())
+                                {
+                                    KickPlayer(p);
+                                }
+                                SaveWorldsAndShutdown();
+                        }
+                    }, 200);
+
                     return true;
                 }
 
@@ -94,13 +121,11 @@ public class StopIt extends JavaPlugin
                         _isInProcess = true;
                         _countdownInMin = Integer.valueOf(args[0]).intValue();
                         _reason = args[1].toString();
-                        _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                        _server.broadcastMessage(ChatColor.WHITE + " Liebe Spieler, der Server wird ");
-                        _server.broadcastMessage(ChatColor.WHITE + " in " + ChatColor.RED + _countdownInMin + ChatColor.WHITE + " Minute(n) neugestartet... ");
-                        _server.broadcastMessage(ChatColor.WHITE + " Grund: " + ChatColor.RED + _reason);
-                        _server.broadcastMessage(ChatColor.WHITE + " Wir sind in wenigen Minuten wieder für euch da!");
-                        _server.broadcastMessage(ChatColor.WHITE + " Sollten Fragen auftregen: TS3-IP: " + ChatColor.RED + this.getConfig().getString("StopIt.commands.ts3ip.messages.ts3ip"));
-                        _server.broadcastMessage(ChatColor.GOLD + "~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        PostAnnouncementOne();
+                        if(_shouldWhiteListBeEnabled)
+                        {
+                            SetWhitelist();
+                        }
                     }
                     catch (Exception ex)
                     {
@@ -117,20 +142,14 @@ public class StopIt extends JavaPlugin
                         _server.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 
                             public void run() {
-                                _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                                _server.broadcastMessage(ChatColor.RED + "Achtung " + ChatColor.WHITE + "der Server startet in kurzer Zeit neu.");
-                                _server.broadcastMessage(ChatColor.WHITE + "Ihr werdet automatisch gekickt.");
-                                _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                PostRepeatingAnnouncement();
                             }
                         }, (_countdownInMin-1)*60*20+100, 400L);
 
                         _server.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 
                             public void run() {
-                                _server.broadcastMessage(ChatColor.RED + "Der Server wird nun heruntergefahren!");
-                                _server.broadcastMessage(ChatColor.GOLD + "Besucht uns solang gern auf unserer Homepage!");
-                                _server.broadcastMessage(ChatColor.AQUA + getConfig().getString("StopIt.commands.homepage.messages.homepage"));
-                                _server.broadcastMessage(ChatColor.WHITE + "Bis gleich! Euer " + getConfig().getString("StopIt.commands.servername.messages.servername") + " Team!");
+                                AnnounceShutdown();
                                 playersShouldBeKicked = true;
                             }
                         }, _countdownInMin*60*20-60);
@@ -138,36 +157,21 @@ public class StopIt extends JavaPlugin
                         _server.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 
                             public void run() {
-                                if(playersShouldBeKicked)
-                                {
 
-                                    if(getConfig().getString("StopIt.commands.excludeplayer.messages.excludeplayer").contains(",") ||
-                                            getConfig().getString("StopIt.commands.excludeplayer.messages.excludeplayer").length() > 0)
-                                    {
-                                        String actualName = getConfig().getString("StopIt.commands.excludeplayer.messages.excludeplayer");
+                                AnnounceShutdown();
 
-                                        String[] split= actualName.split(",");
-                                        _excluded = split;
-                                    }
-                                    else
-                                    {
-                                        String[] test = {""};
-                                        _excluded = test;
+                                if (playersShouldBeKicked) {
+
+                                    for (Player p : _server.getOnlinePlayers()) {
+                                        KickPlayer(p);
                                     }
 
-                                    KickNonExludedPlayer();
-                                    _server.broadcastMessage(ChatColor.RED + "Welten und Inventare werden gespeichert...");
-                                    _server.dispatchCommand(ccs, "save-all");
-                                    _isInProcess = false;
-
-                                    if(getConfig().getString("StopIt.commands.autostopper.messages.autostopper") == "true") _server.shutdown();
-
+                                    SaveWorldsAndShutdown();
                                 }
                             }
-                        }, _countdownInMin*60*20 + 60);
+                        }, _countdownInMin * 60 * 20 + 60);
 
                         return true;
-
                     }
                     else
                     {
@@ -181,46 +185,63 @@ public class StopIt extends JavaPlugin
 
             return false;
 
-
+        }
+        return false;
     }
 
-    private void KickNonExludedPlayer() {
-        for (Player onlinePlayer : _server.getOnlinePlayers())
+    private void SetWhitelist()
+    {
+        _server.setWhitelist(true);
+    }
+
+    private void AnnounceShutdown() {
+        _server.broadcastMessage(ChatColor.RED + "Der Server wird nun heruntergefahren!");
+        _server.broadcastMessage(ChatColor.GOLD + "Besucht uns solang gern auf unserer Homepage!");
+        _server.broadcastMessage(ChatColor.AQUA + getConfig().getString("StopIt.commands.homepage.messages.homepage"));
+        _server.broadcastMessage(ChatColor.WHITE + "Bis gleich! Euer " + getConfig().getString("StopIt.commands.servername.messages.servername") + " Team!");
+    }
+
+    private void PostRepeatingAnnouncement() {
+        _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        _server.broadcastMessage(ChatColor.RED + "Achtung " + ChatColor.WHITE + "der Server startet in kurzer Zeit neu.");
+        _server.broadcastMessage(ChatColor.WHITE + "Ihr werdet automatisch gekickt.");
+        _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    private void PostAnnouncementOne() {
+        _server.broadcastMessage(ChatColor.GOLD + "\n\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        _server.broadcastMessage(ChatColor.WHITE + " Liebe Spieler, der Server wird ");
+        _server.broadcastMessage(ChatColor.WHITE + " in " + ChatColor.RED + _countdownInMin + ChatColor.WHITE + " Minute(n) neugestartet... ");
+        _server.broadcastMessage(ChatColor.WHITE + " Grund: " + ChatColor.RED + _reason);
+        _server.broadcastMessage(ChatColor.WHITE + " Wir sind in wenigen Minuten wieder für euch da!");
+        _server.broadcastMessage(ChatColor.WHITE + " Sollten Fragen auftregen: TS3-IP: " + ChatColor.RED + this.getConfig().getString("StopIt.commands.ts3ip.messages.ts3ip"));
+        _server.broadcastMessage(ChatColor.GOLD + "~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    private void SaveWorldsAndShutdown() {
+        _server.broadcastMessage(ChatColor.RED + "Welten und Inventare werden gespeichert...");
+        _server.dispatchCommand(ccs, "save-all");
+        _isInProcess = false;
+
+        if(getConfig().getBoolean("StopIt.commands.autostopper.messages.autostopper"))
         {
-            if(_excluded[0].length()>0)
-            {
-                int amountExcl = _excluded.length;
-                boolean isPlayerInsideExludes = false;
-                for(int i = 0; i < amountExcl; i++)
-                {
-                    ccs.sendMessage(onlinePlayer.getDisplayName() + " " + _excluded[amountExcl-1].toString());
-                    if(onlinePlayer.getDisplayName() == _excluded[amountExcl-1].toString())
-                    {
-                        ccs.sendMessage(onlinePlayer.getDisplayName() + " wird ignoriert.");
-                        isPlayerInsideExludes = true;
-                    }
-                }
-
-                if(isPlayerInsideExludes)
-                {
-                    ccs.sendMessage(onlinePlayer.getDisplayName() + " ignoriert.");
-                }
-                else {
-
-                    onlinePlayer.kickPlayer(ChatColor.GOLD + "Alle wurden wegen eines Restarts ausgeloggt. Bis gleich hoffentlich! :)\n" +
-                            ChatColor.GREEN + "Besuch uns gern auf " + getConfig().getString("StopIt.commands.homepage.messages.homepage") + "\n" +
-                            ChatColor.AQUA + "Oder auf dem TS3: " + getConfig().getString("StopIt.commands.ts3ip.messages.ts3ip"));
-                }
-            }
-            else
-            {
-                onlinePlayer.kickPlayer(ChatColor.GOLD + "Alle wurden wegen eines Restarts ausgeloggt. Bis gleich hoffentlich! :)\n" +
-                        ChatColor.GREEN + "Besuch uns gern auf " + getConfig().getString("StopIt.commands.homepage.messages.homepage") + "\n" +
-                        ChatColor.AQUA + "Oder auf dem TS3: " + getConfig().getString("StopIt.commands.ts3ip.messages.ts3ip"));
-            }
+            _server.shutdown();
         }
     }
 
+    private void KickPlayer(Player p)
+    {
+        if(p.hasPermission("stopit.ignoreOnKick"))
+        {
+            p.sendMessage("Du wurdest nicht gekickt.");
+        }
+        else
+        {
+            p.kickPlayer(ChatColor.GOLD + "Alle wurden wegen eines Restarts ausgeloggt. Bis gleich hoffentlich! :)\n" +
+                    ChatColor.GREEN + "Besuch uns gern auf " + getConfig().getString("StopIt.commands.homepage.messages.homepage") + "\n" +
+                    ChatColor.AQUA + "Oder auf dem TS3: " + getConfig().getString("StopIt.commands.ts3ip.messages.ts3ip"));
+        }
+    }
 
     @Override
 	public void onDisable()
